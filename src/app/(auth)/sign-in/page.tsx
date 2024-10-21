4cc78501bc2f0fd02042cdb83b777121af712199
masterthuku <peterthuku706@gmail.com>
2024-10-21 04:37:25 +0300

    first commit

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import { SignInCard } from "@/features/auth/components/SignInCard"
+
+
+const SignInPage = () => {
+  return (
+    <SignInCard/>
+  )
+}
+
+export default SignInPage
---
+++ src/features/auth/components/SignInCard.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+export const SignInCard = () => {
+  return (
+    <Card className="w-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Welcome Back 🎉</CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <form className="space-y-4">
+          <Input required type="email" value={""} onChange={() => {}} />
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
