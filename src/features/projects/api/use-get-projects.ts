e2b6089e149196f2b247d51c18f835de0291696d
masterthuku <peterthuku706@gmail.com>
2024-11-07 13:16:40 +0300

    update

---
+++ src/features/projects/api/use-get-projects.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { client } from "@/lib/rpc";
+import { useQuery } from "@tanstack/react-query";
+
+interface useGetProjectsProps {
+    workspaceId: string
+}
+
+export const useGetProjects = ({workspaceId}: useGetProjectsProps) => {
+  const query = useQuery({
+    queryKey: ["projects", workspaceId],
+    queryFn: async () => {
+      const response = await client.api.projects.$get({
+        query: { workspaceId },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch projects");
+      }
+
+      const { data } = await response.json();
+
+      return data;
+    },
+  });
+  return query;
+};
