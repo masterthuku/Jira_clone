3a940694090e793e7087c331921f23710a22d24d
masterthuku <peterthuku706@gmail.com>
2024-11-10 03:51:45 +0300

    project level

---
+++ src/features/projects/queries.ts
@@ -0,0 +1,36 @@
+import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { getMember } from "../members/utils";
+import { createSessionClient } from "@/lib/appwrite";
+import { Project } from "./types";
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+interface GetProjectProps {
+  projectId: string;
+}
+
+export const getProject = async ({ projectId }: GetProjectProps) => {
+  try {
+    const { databases, account } = await createSessionClient();
+    const user = await account.get();
+
+    const project = await databases.getDocument<Project>(
+      DATABASE_ID,
+      PROJECTS_ID,
+      projectId
+    );
+
+    const member = await getMember({
+      databases,
+      userId: user.$id,
+      workspaceId: project.workspaceId,
+    });
+
+    if (!member) {
+      return null;
+    }
+
+    return project;
+  } catch {
+    return null;
+  }
+};
